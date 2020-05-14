820801a114a0c7de5d0161839af7d4fc89d315e8
mohanson <mohanson@outlook.com>
2020-05-14 15:22:17 +0800

    Submit for stash 03

--- src/main.rs
+++ src/main.rs
@@ -101,12 +101,12 @@ impl Interpreter {
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     assert!(args.len() >= 2);
-    let mut f = std::fs::File::open(&args[1]).unwrap();
+    let mut f = std::fs::File::open(&args[1])?;
     let mut c: Vec<u8> = Vec::new();
-    f.read_to_end(&mut c).unwrap();
-    let mut it = Interpreter::default();
-    it.run(c).unwrap();
+    f.read_to_end(&mut c)?;
+    let mut i = Interpreter::default();
+    i.run(c)
 }
